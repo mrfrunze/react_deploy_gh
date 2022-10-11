9a9bacf0e142f271a52f46411d418e6f79af2afe
mrfrunze <stoynov.oleg@gmail.com>
2022-10-11 22:10:59 +0300

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -13,34 +13,13 @@ function App() {
   const [newItem, setNewItem] = useState('');
   const [seachItem, setSeachItem] = useState('');
 
-  // useEffect следит за изминениями в массиве - который передается вторым парметром!
-  // useEffect - смотрит на зависимости!! в дам случаи items
-  // useEffect рабоате иссинхронно и выполняется после всех функций 
-
-  // console.log("before loading");
   useEffect(() => {
-    // console.log('update items list');
-    // useEffect оторабыват только тогда когда есть изминения в массиве!
-    // если вводит вполе поиска то будет отробатывать только before and after!
     console.log('insted loading');
   }, [items])
 
-  // console.log("after loading");
-
-  // добавим в useState JSON.parse(localStorage.getItem('shoppinglist')) || []
-  // если в Aplication удалить масив - будет ошибка это значит что filter = null, поэтому по умолчанию должен быть пустой массви! 
-
-  // перенесем localStorage.setItem('shoppinglist', JSON.stringify(newItems)) в useEffect и заменим newItems на items - так как нам нужно сделить за изминениями именно items!
   useEffect(() => {
     localStorage.setItem('shoppinglist', JSON.stringify(items))
   }, [items])
-
-
-  //  удалми setAndSaveItems - он нам болше не нужен! 
-  // заменим setAndSaveItems на setItems
-  // const setAndSaveItems = (newItems) => {
-  //   setItems(newItems)
-  // }
 
   const addItem = item => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -72,6 +51,7 @@ function App() {
   return (
     <div className="App">
       <Header title="To do List"/>
+      <main>
       <Additem  
         newItem={newItem}
         setNewItem={setNewItem}
@@ -86,6 +66,7 @@ function App() {
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
+      </main>
      <Footer length={items.length} />
     </div>
   );
